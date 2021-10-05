dbb7ccc3232f76084316a506f79753ee99cc0d3b
shubhampatte <shubhampatte@gmail.com>
2021-10-05 17:12:08 +0530

    [shubham]:TwoDArray program Added

--- TwoDArray.java
+++ TwoDArray.java
@@ -4,33 +4,47 @@ public class TwoDArray
 {
 	public static void main(String args[])
 	{
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Enter Row length of an array : ");
-	int row = sc.nextInt();
-	System.out.println("Enter column length of an array : ");
-	int column = sc.nextInt();
-	int a[][]= new int[row][column];    	 
-	System.out.print("Enter " + row*column + " Elements to Store in Array : ");
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter Row length of an array : ");
+		int row = sc.nextInt();
+		System.out.println("Enter column length of an array : ");
+		int column = sc.nextInt();
+	
+		int a[][]= new int[row][column];
+	
+		System.out.println("Enter " + row*column + " Elements to Store in Array ");
 	
 		for (int i = 0; i < row; i++)
 		{
 			for(int j = 0; j < column; j++)
 			{
+				
+				System.out.println("Elements in Array are : ");
 				a[i][j] = sc.nextInt();
-				System.out.print("Elements in Array are : ");
-
 			}
-			
-
+				
+				System.out.println(" ");	
+				
 		}	   
         
 		for (int i = 0; i < row; i++)
 		{
 			for(int j = 0; j < column; j++)
 			{
-				System.out.println("Row ["+i+"]:  Column ["+j+"] :" +a[i][j]);
+				System.out.println("Elements in Array are Row ["+i+"]:  Column ["+j+"] :" +a[i][j]);
 			}
-		}		
+				System.out.println(" ");
+		}
+			
+		for (int i = 0; i < row; i++)
+		{
+			for(int j = 0; j < column; j++)
+			{
+			System.out.print(+a[i][j]+ " ");
+			}
+				System.out.println(" ");
+		}	
 
 	}
 
